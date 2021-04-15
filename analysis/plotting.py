62d3b1beb8cace5c624c7e949f08cd54affb9b39
kingusiu <3071710+kingusiu@users.noreply.github.com>
2021-04-15 11:22:43 +0200

    adding plot_latent_space_2D_bg_vs_sig to visualize latent space location of BG and SIG datasamples

--- analysis/plotting.py
+++ analysis/plotting.py
@@ -1,9 +1,24 @@
 import os
 import numpy as np
 import math
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib import colors
 import matplotlib.cm as cm
+
+
+def make_pairwise_heatmaps(data, num_dim):
+    heatmaps = []
+    xedges = []
+    yedges = []
+
+    for i in range(0, num_dim, 2):
+        heatmap, xedge, yedge = np.histogram2d(data[:, i], data[:, i+1], bins=70)
+        heatmaps.append(heatmap)
+        xedges.append(xedge)
+        yedges.append(yedge)
+
+    return np.asarray(heatmaps), np.asarray(xedges), np.asarray(yedges)
 
 
 def plot_latent_space_2D(latent_coords, title_suffix="data", filename_suffix="data", fig_dir='fig'): # ndarray [M x K] 
@@ -14,19 +29,7 @@ def plot_latent_space_2D(latent_coords, title_suffix="data", filename_suffix="da
 
     latent_dim_n = latent_coords.shape[1] - 1 if latent_coords.shape[1] % 2 else latent_coords.shape[1] # if num latent dims is odd, slice off last dim
 
-    heatmaps = []
-    xedges = []
-    yedges = []
-
-    for i in range(0, latent_dim_n, 2):
-        heatmap, xedge, yedge = np.histogram2d(latent_coords[:, i], latent_coords[:, i+1], bins=70)
-        heatmaps.append(heatmap)
-        xedges.append(xedge)
-        yedges.append(yedge)
-
-    heatmaps = np.asarray(heatmaps)
-    xedges = np.asarray(xedges)
-    yedges = np.asarray(yedges)
+    heatmaps, xedges, yedges = make_pairwise_heatmaps(latent_coords, latent_dim_n)    
 
     min_hist_val = np.min(heatmaps[heatmaps > 0])  # find min value for log color bar clipping zero values
     max_hist_val = np.max(heatmaps)
@@ -57,7 +60,7 @@ def plot_latent_space_2D(latent_coords, title_suffix="data", filename_suffix="da
     plt.close(fig)
 
 
-def plot_latent_space_2D_bg_vs_sig(latent_bg, sig_latent, title_suffix="data", filename_suffix="data", fig_dir='fig'):
+def plot_latent_space_2D_bg_vs_sig(latent_bg, latent_sig, title_suffix="data", filename_suffix="data", fig_dir='fig'):
     '''
         plot latent space coordinates by slices of 2D
         latent_coords ... ndarray of sim [M x K] containing M samples with each K latent coordinates
@@ -65,37 +68,28 @@ def plot_latent_space_2D_bg_vs_sig(latent_bg, sig_latent, title_suffix="data", f
 
     latent_dim_n = latent_bg.shape[1] - 1 if latent_bg.shape[1] % 2 else latent_bg.shape[1] # if num latent dims is odd, slice off last dim
 
-    heatmaps_bg = []
-    xedges_bg = []
-    yedges_bg = []
-    heatmaps_sig = []
-    xedges_sig = []
-    yedges_sig = []
+    heatmaps_bg, xedges_bg, yedges_bg = make_pairwise_heatmaps(latent_bg, latent_dim_n)
+    heatmaps_sig, xedges_sig, yedges_sig = make_pairwise_heatmaps(latent_sig, latent_dim_n)
 
-    for i in range(0, latent_dim_n, 2):
-        heatmap, xedge, yedge = np.histogram2d(latent_bg[:, i], latent_bg[:, i+1], bins=70)
-        heatmaps_bg.append(heatmap)
-        xedges_bg.append(xedge)
-        yedges_bg.append(yedge)
+    min_hist_val = min(np.min(heatmaps_bg[heatmaps_bg > 0]), np.min(heatmaps_sig[heatmaps_sig > 0]))  # find min value for log color bar clipping zero values
+    max_hist_val = max(np.max(heatmaps_bg), np.max(heatmaps_sig))
 
-    heatmaps = np.asarray(heatmaps)
-    xedges = np.asarray(xedges)
-    yedges = np.asarray(yedges)
-
-    min_hist_val = np.min(heatmaps[heatmaps > 0])  # find min value for log color bar clipping zero values
-    max_hist_val = np.max(heatmaps)
-
-    extent = [np.min(xedges), np.max(xedges), np.min(yedges), np.max(yedges)]
+    extent = [min(np.min(xedges_bg), np.min(xedges_sig)), max(np.max(xedges_bg), np.max(xedges_sig)), \
+              min(np.min(yedges_bg), np.min(yedges_sig)), max(np.max(yedges_bg), np.max(yedges_sig))]
 
     # calculate number of subplots on canvas
     nrows = int(round(math.sqrt(latent_dim_n/2)))
     ncols = math.ceil(math.sqrt(latent_dim_n/2))
 
     fig, axs = plt.subplots(nrows=nrows, ncols=ncols, figsize=(9, 9))
+    # make dark controur lines
+    levels_n = 6
+    colors_bg = [mpl.colors.rgb2hex(cm.get_cmap('Blues')(int(i))) for i in np.linspace(60, 350, levels_n)]
+    colors_sig = [mpl.colors.rgb2hex(cm.get_cmap('Oranges')(int(i))) for i in np.linspace(60, 350, levels_n)]
 
-    for d, ax in zip(range(len(heatmaps)), axs.flat[:latent_dim_n]):
-        im = ax.imshow(heatmaps[d].T, extent=extent, origin='lower',
-                       norm=colors.LogNorm(vmin=min_hist_val, vmax=max_hist_val))
+    for d, (heat_bg, heat_sig, ax) in enumerate(zip(heatmaps_bg, heatmaps_sig, axs.flat[:latent_dim_n])):
+        cont_bg = ax.contour(heat_bg.T, extent=extent, colors=colors_bg, levels=levels_n)
+        cont_sig = ax.contour(heat_sig.T, extent=extent, colors=colors_sig, levels=levels_n)
         ax.set_title('dims {} & {}'.format(d*2, d*2+1), fontsize='small')
         # fig.colorbar(im, ax=ax)
 
@@ -103,9 +97,16 @@ def plot_latent_space_2D_bg_vs_sig(latent_bg, sig_latent, title_suffix="data", f
     if axs.size > latent_dim_n/2:
         for a in axs.flat[int(latent_dim_n/2):]: a.axis('off')
 
+    plt.legend([cont_bg.collections[0], cont_sig.collections[0]], ['Background', 'Signal'], loc='center')
     plt.suptitle('latent space ' + title_suffix)
-    cb = fig.colorbar(im)
-    cb.set_label('count')
     plt.tight_layout()
     fig.savefig(os.path.join(fig_dir, 'latent_space_2D_contour_' + filename_suffix + '.png'))
     plt.close(fig)
+
+
+def plot_kmeans(data, cluster_assignemnts, cluster_centers):
+
+    latent_dim_n = data.shape[1] - 1 if data.shape[1] % 2 else data.shape[1] # if num latent dims is odd, slice off last dim
+
+    for i in range(0, latent_dim_n, 2):
+        plt.scatter(data[:,i], data[:,i+1], c=cluster_assignemnts, cmap='')
--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 import setGPU
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import datetime
+import pathlib
+import tensorflow as tf
 
 import inference.train_autoencoder as train
 import inference.predict_autoencoder as pred
 import inference.kmeans as kmeans
 import analysis.plotting as plot
 
-# train AE model
-ae_model = train.train('qcdSide', epochs=20)
 
-# model save /load 
-# ae_model.save(...) # calls native keras function
-# ae_model.load(...) # calls overriding load fun with custom objects
+def make_model_path(date=None):
+    date_str = ''
+    if date is None:
+        date = datetime.date.today()
+        date = '{}{:02d}{:02d}'.format(date.year, date.month, date.day)
+    path = os.path.join('models/saved', 'AEmodel_{}'.format(date))
+    pathlib.Path(path).mkdir(parents=True, exist_ok=True)
+    return path
 
+load = True
+model_path = make_model_path('20210414')
+
+if load:
+    ae_model = tf.saved_model.load(model_path)
+
+else:
+    # train AE model
+    ae_model = train.train('qcdSide', epochs=20)
+
+    # model save /load
+    tf.saved_model.save(ae_model, model_path) 
 
 # apply AE model
 sample_id = 'qcdSideExt'
@@ -22,8 +40,7 @@ sample_id = 'GtoWW35na'
 latent_coords_sig = pred.map_to_latent_space(sample_id, ae_model)
 
 # plot latent space
-plot.plot_latent_space_2D(latent_coords_qcd, title_suffix="qcd", filename_suffix="qcd", fig_dir='fig')
-plot.plot_latent_space_2D(latent_coords_sig, title_suffix="sig", filename_suffix="sig", fig_dir='fig')
+plot.plot_latent_space_2D_bg_vs_sig(latent_coords_qcd, latent_coords_sig, title_suffix="qcd vs G_RS", filename_suffix="qcd_vs_sig", fig_dir='fig')
 
 # train k-means model
 print('>>> training kmeans')
